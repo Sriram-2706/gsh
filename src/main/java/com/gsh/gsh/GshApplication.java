856e8d869d13a86c5b6336332b4493ec7beac15b
Sriram Nagarajan <nsri.1340@gmail.com>
2026-03-31 12:03:17 +0530

    first commit

---
+++ src/main/java/com/gsh/gsh/GshApplication.java
@@ -0,0 +1,13 @@
+package com.gsh.gsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsh/gsh/GshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsh.gsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
